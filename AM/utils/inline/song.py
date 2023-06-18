66791da3585f6f770e9c80261b5b251d1dff0408
Alex <127078192+ALEX665E@users.noreply.github.com>
2023-06-19 03:43:01 +0530

    Update song.py

--- AM/utils/inline/song.py
+++ AM/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_GROUP}",
+                text=" sᴜᴩᴩᴏʀᴛ ", url= "https://t.me/MahaKaL_Ki_LadLi",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
